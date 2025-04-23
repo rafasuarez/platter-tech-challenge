e6a2072389fb0ad43fa5085ae986aa964431c308
Rafa Suarez <rafael@grupocartagena.com>
2025-04-23 09:59:25 -0500

    WIP

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,4 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const loadMorebutton = document.querySelector('.js-toggle-button')
+    const carouselContainer = document.querySelector('.js-carousel-element')
 
-  })
-  
+    if (!loadMorebutton || !(carouselContainer instanceof HTMLElement)) {
+        return
+    }
+
+
+    const setupToggleButton = () => {
+        let expanded = false
+        loadMorebutton.addEventListener('click', () => {
+            expanded = !expanded
+
+            carouselContainer.classList.toggle('grid-rows-[1fr_1fr_0fr_0fr_0fr]', !expanded)
+            carouselContainer.classList.toggle('grid-rows-[1fr_1fr_1fr_1fr_1fr]', expanded)
+            loadMorebutton.textContent = expanded ? 'Show Less' : 'Show More'
+        })
+    }
+    setupToggleButton()
+})  
